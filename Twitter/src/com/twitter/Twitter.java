73062f5fae0bb818f131c433d5d200358a7744c6
anadzudovic <anadzudovic1@gmail.com>
2017-04-05 10:05:04 +0200

    Napisana JavaDoc dokumentacija za klasu Twitter i njene atribute i metode.

--- Twitter/src/com/twitter/Twitter.java
+++ Twitter/src/com/twitter/Twitter.java
@@ -3,17 +3,48 @@ package com.twitter;
 import java.util.LinkedList;
 
 import com.twitter.poruke.TwitterPoruka;
+/**
+ * Klasa Twitter predstavlja listu twitter poruka.
+ * @author anadzudovic
+ *@version 1.0
+ */
+
 public class Twitter {
+	/**
+	 * Atribut poruke predstavlja listu Twitterporuka.
+	 */
 	private LinkedList<TwitterPoruka> poruke = new LinkedList<TwitterPoruka>();
+	/**
+	 * Ova metoda vraca sve poruke iz liste poruka.
+	 * @return poruke kao listu Twitterporuka.
+	 */
 	public LinkedList<TwitterPoruka> vratiSvePoruke(){ 
 		return poruke; 
 		} 
+	/**
+	 * Ova metoda ubacuje Twitterporuku na poslednje mesto u listi poruka.
+	 * @param korisnik predstavlja korisnika koji unosi novu poruku 
+	 * @param poruka predstavlja tekst poruke koja se unosi
+	 */
+	
 	public void unesi(String korisnik, String poruka) { 
         TwitterPoruka tp = new TwitterPoruka(); 
         tp.setKorisnik("korisnik");
         tp.setPoruka(poruka);  
         poruke.addLast(tp); 
         }  
+	/**
+	 * Ova metoda pretrazuje listu poruka i vraca niz poruka koje sadrze odredjeni tag, koji se unesi kao parametar.
+	 * Kapacitet ovog niza, tj koliko ce najvise poruka sa tagom ova metoda vratiti se takodje unosi kao parametar.
+	 * @param maxBroj predstavlja kapacitet niza, tj koliko ce se najvise poruka vratiti
+	 * @param tag predstavlja kriterijum na osnovu kojeg se pretrazuje lista poruka
+	 * @return rezultat koji predstavlja niz poruka sa unetim tagom
+	 * @throws java.lang.RuntimeException.class kada:
+	 * <ul>
+	 * <li> je tag null</li>
+	 * <ll> je tag prazan String </li>
+	 * </ul>
+	 */
 	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) { 
 		if (tag==null || tag.isEmpty()) throw new RuntimeException("Morate uneti tag");
 		 if (maxBroj<=0) maxBroj = 100;     
